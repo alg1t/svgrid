21e759ff807d8f566c79db71f1b931ef2dea5922
Alg1t <alfell88@googlemail.com>
2022-12-29 17:47:46 +0000

    dec28

--- script.js
+++ script.js
@@ -1,23 +1,46 @@
-
-
 function reportWindowSize() {
-  // heightOutput.textContent = window.innerHeight;
-  // widthOutput.textContent = window.innerWidth;
+
   const shape = document.getElementById("galleryHeader");
+
   if (window.innerWidth <= 400) {
-      shape.setAttribute("viewBox", "-65 0 550 100");
-    } else if (window.innerWidth <= 600 ) {
-      shape.setAttribute("viewBox", "-120 0 650 100");
-    } else if (window.innerWidth <= 900 ) {
-        shape.setAttribute("viewBox", "-160 0 750 100");
-      
+    shape.setAttribute("viewBox", "-65 0 550 100");
+  } else if (window.innerWidth <= 600) {
+    shape.setAttribute("viewBox", "-120 0 650 100");
+  } else if (window.innerWidth <= 900) {
+    shape.setAttribute("viewBox", "-160 0 750 100");
   } else {
     shape.setAttribute("viewBox", "-460 0 1350 100");
   }
 }
+window.onload = reportWindowSize();
 
 
+// const shape = document.getElementById("galleryHeader");
 
 
+// function createFiller() {
+//   const svg = document.createElement('div');
+//   svg.classList.add('headerFillerTest');
+//   svg.innerHTML = ('hello');
+//   header.append(svg);
+//   return svg;
+// }
+const headerObs = document.getElementById("galleryContainer");
 
-window.onload = reportWindowSize;
+const loadImg = function (entries) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+  //  else alert("intercepting");
+  else
+    // window.onload = reportWindowSize();
+    $("#galleryHeader").load(location.href + " #galleryHeader>*", "");
+};
+
+const headerObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '-100px',
+});
+
+headerObserver.observe(headerObs);
